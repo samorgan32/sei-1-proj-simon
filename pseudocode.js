c118d8e8a7993ae16ee27702572c09a985c8ee1f
samorgan32 <samorgan32@gmail.com>
2021-04-30 11:13:34 -0500

    block flashes on page load

--- pseudocode.js
+++ pseudocode.js
@@ -17,26 +17,30 @@ gridBlock2.addEventListener('click', (event) => {
 
 // function to revert style back after click 
 function revertBackground(event) {
-    gridBlock1.style.background = '#ff9980'
-    gridBlock2.style.background = '#ff9980'
+    gridBlock1.style.background = '#92d6c3'
+    gridBlock2.style.background = '#92d6c3'
 }
 
 //generate random sequence
 //assign each block either 0 or 1, generate random 0 or 1, 
 
-const sequenceButton = document.querySelector('#new-game')
+const sequenceButton = document.querySelector('#initiate')
 
 sequenceButton.addEventListener('click', (event) => {
-    // console.log(event.target)
     for (let i = 2; i < 3; i++) {
         let num = Math.floor(Math.random() * 2)
         sequence.push(num)
-        // console.log(num)
-
     }
-
     console.log(sequence)
 })
 
-//each click adds one random '0' or '1' to the array, increasing the array by one with each click.
-//
+//each click adds one random '0' or '1' to the array, increasing the array by one
+//assign each a styling to hide or show the block
+//assign each block a set timeout to run on click 
+
+function intiateSequence() {
+    gridBlock1.style.background = 'white'
+    setTimeout(revertBackground, 120)
+}
+
+intiateSequence()
