013a868b82423fc7652052e4860531e9bc7cdb07
samorgan32 <samorgan32@gmail.com>
2021-04-29 19:24:29 -0500

    working on sequence generator

--- pseudocode.js
+++ pseudocode.js
@@ -5,13 +5,11 @@ const gridBlock2 = document.querySelector('.gb2')
 
 
 gridBlock1.addEventListener('click', (event) => {
-    // console.log(event.target)
     event.target.style.background = '#ff9980'
     setTimeout(revertBackground, 120)
 })
 
 gridBlock2.addEventListener('click', (event) => {
-    // console.log(event.target)
     event.target.style.background = '#ff9980'
     setTimeout(revertBackground, 120)
 })
@@ -21,3 +19,7 @@ function revertBackground(event) {
     gridBlock1.style.background = 'white'
     gridBlock2.style.background = 'white'
 }
+
+//generate random sequence
+//assign each block either 0 or 1, generate random 0 or 1, 
+
